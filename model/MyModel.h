d2427472e9ccf68e5d03318efd32605a8f767c9e
yinwentao <1376836598@qq.com>
2016-04-19 11:12:18 +0800

    mymodel

---
+++ model/MyModel.h
@@ -0,0 +1,13 @@
+//
+//  MyModel.h
+//  RunTimeDEMO
+//
+//  Created by 尹文涛 on 16/4/19.
+//  Copyright © 2016年 小木科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyModel : NSObject
+
+@end
---
+++ model/MyModel.m
@@ -0,0 +1,13 @@
+//
+//  MyModel.m
+//  RunTimeDEMO
+//
+//  Created by 尹文涛 on 16/4/19.
+//  Copyright © 2016年 小木科技. All rights reserved.
+//
+
+#import "MyModel.h"
+
+@implementation MyModel
+
+@end
